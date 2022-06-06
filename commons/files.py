db0a109d77267fa604dd1e0d6ca61b3422f48038
Amir Sharapov <amir@amirfounder.com>
2022-06-05 21:47:13 -0500

    write dict as json

--- commons/files.py
+++ commons/files.py
@@ -1,22 +1,28 @@
 import json
 
 
-def safe_read(path, mode='r', encoding='utf-8', log_fn=print):
-    kwargs = {
+def format_exception_caught_message(e: Exception):
+    return f'Caught {type(e).__name__} Exception: {str(e)}'
+
+
+def safe_read(path, mode='r', encoding='utf-8', *args, log_on_failure=True, log_fn=print, **kwargs):
+    kwargs.update({
         'file': path,
         'mode': mode,
         'encoding': encoding
-    }
+    })
 
     if mode.endswith('b'):
         kwargs.pop('encoding')
 
     try:
-        with open(**kwargs) as f:
+        with open(*args, **kwargs) as f:
             return f.read()
 
     except Exception as e:
-        log_fn(f'Caught {type(e).__name__} Exception: {str(e)}')
+        if log_on_failure:
+            msg = format_exception_caught_message(e)
+            log_fn(msg)
 
 
 def read_json(path, default=None):
@@ -31,3 +37,20 @@ def read_json(path, default=None):
         result = json.loads(result)
 
     return result
+
+
+def write_dict_as_json(path, dict_obj, log_on_failure=True, log_fn=print, *args, **kwargs):
+    if 'indent' not in kwargs:
+        kwargs['indent'] = 4
+    if 'sort_keys' not in kwargs:
+        kwargs['sort_keys'] = True
+
+    try:
+        json_obj = json.dumps(dict_obj, *args, **kwargs)
+        with open(path, 'w') as f:
+            f.write(json_obj)
+
+    except Exception as e:
+        if log_on_failure:
+            msg = format_exception_caught_message(e)
+            log_fn(msg)
