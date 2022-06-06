0f6af9d2693e5f9288a4284b78c7baa259c53f2b
Amir Sharapov <amir@amirfounder.com>
2022-06-06 17:15:46 -0500

    removed cached files

--- tests/test_logging_handlers.py
+++ tests/test_logging_handlers.py
@@ -2,7 +2,7 @@ from commons.logging_handlers import log_error, configure_path
 
 
 def test_log_error_happy_path():
-    configure_path('test-data/logs.txt')
+    configure_path('tests/dummy-data/logs.txt')
     log_error('This is a test')
     configure_path(None)
 
