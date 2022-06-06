280833872dc267dfaa43df5c24fe9ef92362462e
Amir Sharapov <amir@amirfounder.com>
2022-06-06 00:31:59 -0500

    run in sep thread fn

--- commons/thread_handlers.py
+++ commons/thread_handlers.py
@@ -2,12 +2,11 @@ from threading import Thread as _Thread
 
 
 class ThreadWrapper:
-    def __init__(self, target, daemon=True, *args, **kwargs):
+    def __init__(self, target, *args, **kwargs):
         self.result = None
         self._target = self._build_target_fn(target)
         self.thread = _Thread(
             target=self._target,
-            daemon=daemon,
             *args,
             **kwargs
         )
@@ -16,3 +15,17 @@ class ThreadWrapper:
         def inner(*args, **kwargs):
             self.result = func(*args, **kwargs)
         return inner
+
+
+def run_in_separate_thread(target, args=None, kwargs=None, *thread_wrapper_args, **thread_wrapper_kwargs):
+    wrapper = ThreadWrapper(
+        target=target,
+        args=args,
+        kwargs=kwargs,
+        daemon=True,
+        *thread_wrapper_args,
+        **thread_wrapper_kwargs
+    )
+    wrapper.thread.start()
+    return wrapper
+
