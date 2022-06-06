944ea78023bcaf2b1ee198f4612b0a73bfe46c01
Amir Sharapov <amir@amirfounder.com>
2022-06-06 17:17:23 -0500

    env handlers

--- commons/env_handlers.py
+++ commons/env_handlers.py
@@ -1,50 +1,50 @@
 from os import environ
 
 
-DEV = 'dev'
-PROD = 'prod'
-STAGING = 'staging'
-QA = 'qa'
-ENV_KEY = 'ENVIRONMENT'
+_DEV = 'dev'
+_PROD = 'prod'
+_STAGING = 'staging'
+_QA = 'qa'
+_ENV_KEY = 'ENVIRONMENT'
 
 
 def configure_environment_key(key):
-    global ENV_KEY
-    ENV_KEY = key
+    global _ENV_KEY
+    _ENV_KEY = key
 
 
 def set_env_to_prod():
-    environ[ENV_KEY] = PROD
+    environ[_ENV_KEY] = _PROD
 
 
 def set_env_to_staging():
-    environ[ENV_KEY] = STAGING
+    environ[_ENV_KEY] = _STAGING
 
 
 def set_env_to_qa():
-    environ[ENV_KEY] = QA
+    environ[_ENV_KEY] = _QA
 
 
 def set_env_to_dev():
-    environ[ENV_KEY] = DEV
+    environ[_ENV_KEY] = _DEV
 
 
 def get_working_env(default: str = None):
-    return environ.get(ENV_KEY, default)
+    return environ.get(_ENV_KEY, default)
 
 
 def is_env_prod():
-    return get_working_env() == PROD
+    return get_working_env() == _PROD
 
 
 def is_env_staging():
-    return get_working_env() == STAGING
+    return get_working_env() == _STAGING
 
 
 def is_env_qa():
-    return get_working_env() == QA
+    return get_working_env() == _QA
 
 
 def is_env_dev():
-    return get_working_env() == DEV
+    return get_working_env() == _DEV
 
