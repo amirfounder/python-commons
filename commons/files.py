ce0bb65e94e5b5f32b820e2ef68fb4f7e3db2ee3
Amir Sharapov <amir@amirfounder.com>
2022-06-05 21:41:03 -0500

    read json

--- commons/files.py
+++ commons/files.py
@@ -1,3 +1,6 @@
+import json
+
+
 def safe_read(path, mode='r', encoding='utf-8', log_fn=print):
     kwargs = {
         'file': path,
@@ -14,3 +17,17 @@ def safe_read(path, mode='r', encoding='utf-8', log_fn=print):
 
     except Exception as e:
         log_fn(f'Caught {type(e).__name__} Exception: {str(e)}')
+
+
+def read_json(path, default=None):
+    if default is None:
+        default = {}
+
+    result = safe_read(path)
+
+    if result is None:
+        result = default
+    else:
+        result = json.loads(result)
+
+    return result
