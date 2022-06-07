bf3de427a0eaf34e95e345eb5a343a2e3674af0d
Amir Sharapov <amir@amirfounder.com>
2022-06-07 12:53:28 -0500

    rename indx in the json index class

--- commons/daos/json_index.py
+++ commons/daos/json_index.py
@@ -8,35 +8,35 @@ from commons.helpers.files import (
 
 
 class AbstractJsonIndex(ABC):
-    def __init__(self, path, index={}, flush_after_put=False, load=True):
+    def __init__(self, path, flush_after_put=False, load=True):
         self.path = path
-        self.index = index
         self.flush_after_put = flush_after_put
+        self.source = None
 
         ensure_path_exists(self.path)
         if load:
             self.load()
 
     def __len__(self):
-        return self.index.__len__()
+        return self.source.__len__()
 
     def __iter__(self):
-        return self.index.__iter__()
+        return self.source.__iter__()
 
     def __contains__(self, o):
-        return self.index.__contains__(o)
+        return self.source.__contains__(o)
 
     def put(self, key, value, **kwargs):
         flush = kwargs['flush'] if 'flush' in kwargs else self.flush_after_put
-        self.index[key] = value
+        self.source[key] = value
         if flush:
             self.flush()
 
     def get(self, key, default=None):
-        return self.index.get(key, default)
+        return self.source.get(key, default)
 
     def load(self):
-        self.index = safe_read_json_as_obj_from_file(self.path, {})
+        self.source = safe_read_json_as_obj_from_file(self.path, {})
 
     def flush(self):
-        safe_write_obj_as_json_to_file(self.path, self.index)
+        safe_write_obj_as_json_to_file(self.path, self.source)
