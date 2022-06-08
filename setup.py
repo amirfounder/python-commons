930bec8ca9a851d97ac5fd560fff860f47dc5555
Amir Sharapov <amir@amirfounder.com>
2022-06-07 22:05:52 -0500

    ver bump and add install requires

--- setup.py
+++ setup.py
@@ -2,7 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='commons',
-    version='0.0.10',
+    version='0.0.11',
     packages=find_packages(),
-    install_requires=[]
+    install_requires=[
+        'mss~=6.1.0',
+        'multipledispatch~=0.6.0'
+    ]
 )
