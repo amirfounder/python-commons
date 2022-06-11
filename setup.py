03051b57eca6d193eb52be782b636aa059355c3f
Amir Sharapov <amir@amirfounder.com>
2022-06-11 18:28:49 -0500

    better json index and model

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='commons',
-    version='0.0.23',
+    version='0.0.24',
     packages=find_packages(),
     install_requires=[
         'mss~=6.1.0',
