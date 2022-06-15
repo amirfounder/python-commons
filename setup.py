a5e4fc03ad8e5233dc282a46ea8a46bb8eabad0b
Amir Sharapov <amir@amirfounder.com>
2022-06-14 19:24:19 -0500

    dependencies

--- setup.py
+++ setup.py
@@ -2,13 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='commons',
-    version='0.0.38',
+    version='0.0.39',
     packages=find_packages(),
     install_requires=[
         'mss~=6.1.0',
         'multipledispatch~=0.6.0',
         'setuptools~=60.2.0',
         'numpy~=1.22.4',
-        'opencv-python~=4.5.5.64'
+        'opencv-python~=4.5.5.64',
+        'pydantic~=1.9.1',
+        'orjson~=3.7.2'
     ]
 )
