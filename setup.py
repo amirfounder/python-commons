59041906b8ab1353feac54b39cf3a6167600b6a8
Amir Sharapov <amir@amirfounder.com>
2022-09-01 00:35:54 -0500

    verbump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='commons',
-    version='0.0.49',
+    version='0.0.50',
     packages=find_packages(),
     install_requires=[
         'mss~=6.1.0',
