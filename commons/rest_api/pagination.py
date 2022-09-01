91ea311677bc3816fe838539974c394ac8dcf2a0
Amir Sharapov <amir@amirfounder.com>
2022-09-01 00:35:39 -0500

    pagination fixed

--- commons/rest_api/pagination.py
+++ commons/rest_api/pagination.py
@@ -1,9 +1,9 @@
 from typing import Generic, TypeVar, Optional, Type
 
-from commons.logging import log_info
 from pydantic import BaseModel, validator
 from pydantic.generics import GenericModel
 
+from commons.logging import log_info
 from commons.rest_api.model_mappers import map_models
 from commons.rest_api.dtos import BaseDTO
 from commons.rest_api.base_model import BaseBLModel
@@ -12,14 +12,19 @@ _T = TypeVar('_T', bound=BaseBLModel)
 
 
 class PaginationOptions(BaseModel):
+    _max_page_size = 100
+
     page: int = 1
     size: int = 20
 
     @validator('size')
     def validate_page(cls, v):
-        if v > 50:
-            log_info(f'Attempted to retrieve more than 50 results ({v}). Defaulting to 50.')
-            v = 50
+        if v > cls._max_page_size:
+            log_info(
+                f'Attempted to retrieve more than {cls._max_page_size} results ({v})... \
+                Defaulting to {cls._max_page_size}.'
+            )
+            v = cls._max_page_size
         return v
 
 
