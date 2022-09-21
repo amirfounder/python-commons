202255565a0f59fbaa759dea86a8e35b5fa314f4
Amir Sharapov <amir@amirfounder.com>
2022-09-20 20:02:30 -0500

    did some changes

--- commons/rest_api/resource_actions.py
+++ commons/rest_api/resource_actions.py
@@ -1,24 +1,4 @@
 from enum import Enum
-from typing import List
-
-
-class ResourceActions(Enum):
-    """
-    This is a list of all the actions that can be performed on a resource.
-    """
-    ALL = 'ALL'
-    READ = 'READ'
-    UPDATE = 'UPDATE'
-    PARTIAL_UPDATE = 'PARTIAL_UPDATE'
-    CREATE = 'CREATE'
-    SOFT_DELETE = 'SOFT_DELETE'
-    HARD_DELETE = 'HARD_DELETE'
-
-    @classmethod
-    def all(cls) -> List:
-        all_ = list(cls)
-        all_.remove(cls.ALL)
-        return all_
 
 
 class SortDirection(Enum):
