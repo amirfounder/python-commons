293acdd12a4273daab6dd9d364d7cfaad1e41571
Amir Sharapov <amir@amirfounder.com>
2022-10-10 18:25:45 -0500

    db session

--- commons/rest_api/route_dependencies.py
+++ commons/rest_api/route_dependencies.py
@@ -1,5 +1,7 @@
 from fastapi import Header, Depends
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from sqlalchemy.engine import Engine
+from sqlalchemy.orm import Session
 
 http_bearer = HTTPBearer(auto_error=False)
 
@@ -20,4 +22,14 @@ def get_bearer_token():
     def dependency(token: HTTPAuthorizationCredentials = Depends(http_bearer)):
         if token:
             return token.credentials
+    return Depends(dependency)
+
+
+def get_db_session(engine: Engine):
+    def dependency():
+        session = Session(engine)
+        try:
+            yield session
+        finally:
+            session.close()
     return Depends(dependency)
