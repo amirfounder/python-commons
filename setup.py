0c4ed1d3a39acd152ea7d66af80f511b86112589
Amir Sharapov <amir@amirfounder.com>
2022-10-11 01:07:41 -0500

    deleted deprecated and updated args on get_on_by_field

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='commons',
-    version='0.0.134',
+    version='0.0.135',
     packages=find_packages(),
     install_requires=[
         'mss~=6.1.0',
