bd25ed77539f88bca2b3292f549d6a5fde7d1446
Amir Sharapov <amir@amirfounder.com>
2022-10-16 19:05:12 -0500

    model mapper type

--- commons/rest_api/model_mappers.py
+++ commons/rest_api/model_mappers.py
@@ -1,12 +1,14 @@
-from typing import Type, Iterable
+from typing import Type, Iterable, TypeVar, List
 
 from pydantic import BaseModel
 
+_T = TypeVar('T', bound=BaseModel)
 
-def map_model(dest_class: Type[BaseModel], model: BaseModel):
+
+def map_model(dest_class: Type[_T], model: BaseModel) -> _T:
     dest_kwargs = model.dict()
     return dest_class(**dest_kwargs)
 
 
-def map_models(dest_class: Type[BaseModel], models: Iterable[BaseModel]):
+def map_models(dest_class: Type[_T], models: Iterable[BaseModel]) -> List[_T]:
     return [map_model(dest_class, model) for model in models]
