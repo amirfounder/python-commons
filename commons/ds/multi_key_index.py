07cbad108ba50bdd14a0391d99c7946258d7da82
Amir Sharapov <amir@amirfounder.com>
2022-10-19 13:03:12 -0500

    extracted multi key and ordered ky index

--- commons/ds/multi_key_index.py
+++ commons/ds/multi_key_index.py
@@ -2,36 +2,6 @@ from collections import defaultdict
 from typing import List
 
 
-class OrderedKeyIndex:
-    def __init__(self):
-        self._keys = []
-        self._data = dict()
-        self.minheap = []
-        self.maxheap = []
-
-    def get(self, key: str):
-        """
-        Returns the value at the specified key or None if the key does not exist. Runs O(1) time.
-        """
-        return self._data.get(key)
-
-    def get_largest(self):
-        """
-        Returns the value a the largest key
-        :return:
-        """
-        pass
-
-    def get_smallest(self):
-        """
-        Returns the value a the smallest key
-        """
-        pass
-
-    def set(self, key, value):
-        pass
-
-
 class MultiKeyIndex:
     """
     A data structure that supports querying your data by multiple keys. A primary key is required (often 'id') and
